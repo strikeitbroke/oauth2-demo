7b1e82ebf28b6e7d90a85e347a8f078eab9904ff
strikeitbroke <strikeitbroke@gmail.com>
2024-12-21 20:22:04 -0800

    first commit frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+
+<script setup>
+
+  const fetchProtected = () => {
+    console.log("fetch protected api endpoint.")
+    axios.get("http://127.0.0.1:8000/api/v1/todo/")
+    .then( res => {
+      console.log("API response: ", res)
+    })
+    .catch ( err => {
+      console.log("error: ", err.response.data)
+    })
+  }
+
+</script>
+<template>
+<button> custom login with Google </button>
+<button @click="fetchProtected"> fetch protected </button>
+</template>
+
